f6b10da2f03a5897af3f9be189749757a51f57b2
Matt Casey <ccasey@ggc.edu>
2019-02-17 19:37:32 -0500

    update Countdown for accuracy

--- app/src/main/java/com/practice/mcasey/myapplication/CountdownTimer/CountdownFragment.java
+++ app/src/main/java/com/practice/mcasey/myapplication/CountdownTimer/CountdownFragment.java
@@ -31,7 +31,7 @@ public class CountdownFragment extends Fragment {
     @BindView(R.id.start_timer) Button mStart;
     CountDownTimer mTimer;
     int mTimeInt;
-    int mMillisInFuture = 0;
+    int mMillisInFuture;
     View mView;
 
     @Nullable
@@ -52,6 +52,8 @@ public class CountdownFragment extends Fragment {
 
     @OnClick(R.id.start_timer)
     public void onStartTimer(){
+        setMillisInFuture(mTimerET.getText().toString());
+        Log.i("FUTURE", String.valueOf(mMillisInFuture));
         mTimer = new CountDownTimer(mMillisInFuture, INTERVAL){
 
             @Override
@@ -61,16 +63,17 @@ public class CountdownFragment extends Fragment {
                 String l_hours = String.valueOf(l/3600000);
                 Log.i("MINUTES", l_minutes);
                 if(l_seconds.length()==5)
-                    mTimerET.setText(l_hours+" : "+l_minutes+" : "+l_seconds.substring(0,2));
+                    mTimerTV.setText(l_hours+" : "+l_minutes+" : "+l_seconds.substring(0,2));
                 else if(l_seconds.length()==3)
-                    mTimerET.setText(l_hours+" : "+l_minutes+" : 00");
+                    mTimerTV.setText(l_hours+" : "+l_minutes+" : 00");
                 else
-                    mTimerET.setText(l_hours+" : "+l_minutes+" : 0"+l_seconds.substring(0,1));
+                    mTimerTV.setText(l_hours+" : "+l_minutes+" : 0"+l_seconds.substring(0,1));
             }
 
             @Override
             public void onFinish() {
-                mTimerET.setText("DONE!!!");
+                mTimerTV.setText("DONE!!!");
+                mStart.setText("Start");
             }
         };
         if(mStart.getText().equals("Start") || mStart.getText().equals("Resume")){
@@ -78,8 +81,8 @@ public class CountdownFragment extends Fragment {
             mTimer.start();
         }
         else if(mStart.getText().equals("Pause")){
-            String pausedTimer = mTimerET.getText().toString();
-            mTimerET.setText(pausedTimer);
+            String pausedTimer = mTimerTV.getText().toString();
+            mTimerTV.setText(pausedTimer);
             mStart.setText("Resume");
             mTimer.cancel();
             //TODO set timer to paused time
@@ -89,6 +92,7 @@ public class CountdownFragment extends Fragment {
     @OnClick(R.id.cancel_timer)
     public void onCancelTimer(){
         mTimer.cancel();
+        mStart.setText("Start");
     }
 
     private void getInput(){
@@ -108,20 +112,19 @@ public class CountdownFragment extends Fragment {
 
             @Override
             public void afterTextChanged(Editable s) {
+
+                //TODO set protocols to not allow out of bounds things (i.e. 65 seconds etc)
                 StringBuilder sb = new StringBuilder(s.toString());
                 boolean updateView = false;
-                Log.i("SB", String.valueOf(sb.length()));
                 if(sb.length() == 3 && !backspace){
                     if(sb.charAt(1) != ':'){
                         sb = sb.insert(1, ':');
-                        Log.i("SB1", String.valueOf(sb.length()));
                         updateView = true;
                     }
                 } else if(sb.length() == 5 && !backspace){
                     if(sb.charAt(1) == ':'){
                         sb = sb.deleteCharAt(1);
                         sb = sb.insert(2, ':');
-                        Log.i("SB2", String.valueOf(sb.length()));
                         updateView = true;
                     }
                 } else if(sb.length() == 6 && !backspace){
@@ -129,22 +132,22 @@ public class CountdownFragment extends Fragment {
                         sb = sb.deleteCharAt(2);
                         sb = sb.insert(1, ':');
                         sb = sb.insert(4, ':');
-                        Log.i("SB3", String.valueOf(sb.length()));
                         updateView = true;
                     }
                 } else if(sb.length() == 7 && !backspace){
                     if(sb.charAt(1) != ':' && sb.charAt(0) != 'D'){
                         sb = sb.deleteCharAt(2).insert(2, ':');
                         sb = sb.deleteCharAt(5).insert(4, ':');
-                        Log.i("SB4", String.valueOf(sb.length()));
                         updateView = true;
                     }
                 } else if(sb.length() == 8 && !backspace){
                     if(sb.charAt(1) == ':'){
                         sb = sb.deleteCharAt(1).insert(2, ':');
                         sb = sb.deleteCharAt(4).insert(5, ':');
-                        Log.i("SB4", String.valueOf(sb.length()));
                         updateView = true;
+                        Log.i("STRING_SEC", sb.toString().substring(sb.toString().length()-2, sb.toString().length()));
+                        Log.i("STRING_MIN", sb.toString().substring(sb.toString().length()-5, sb.toString().length()-3));
+                        Log.i("STRING_HRS", sb.toString().substring(sb.toString().length()-8, sb.toString().length()-6));
                     }
                 }
                 if (updateView) {
@@ -156,9 +159,41 @@ public class CountdownFragment extends Fragment {
                             sbString.deleteCharAt(i);
                     }
                     Log.i("STRING", sbString.toString());
-                    mMillisInFuture = Integer.parseInt(sbString.toString()) * INTERVAL;
+                    mMillisInFuture = Integer.parseInt(sbString.toString());
+                    Log.i("INTEGER", String.valueOf(mMillisInFuture));
+
                 }
             }
         });
+    }
+
+    private void setMillisInFuture(String millisInFutureString){
+        mMillisInFuture = 0;
+        int seconds = 0;
+        int minutes = 0;
+        int hours = 0;
+        if(millisInFutureString.length()==1)
+            seconds = Integer.parseInt(millisInFutureString
+                    .substring(millisInFutureString.length()-1, millisInFutureString.length()));
+        else if(millisInFutureString.length()>=2)
+            seconds = Integer.parseInt(millisInFutureString
+                    .substring(millisInFutureString.length()-2, millisInFutureString.length()));
+        if(millisInFutureString.length()==4)
+            minutes = Integer.parseInt(millisInFutureString
+                    .substring(millisInFutureString.length()-4, millisInFutureString.length()-3));
+        else if(millisInFutureString.length()>=5)
+            minutes = Integer.parseInt(millisInFutureString
+                    .substring(millisInFutureString.length()-5, millisInFutureString.length()-3));
+        if(millisInFutureString.length()==7)
+            hours = Integer.parseInt(millisInFutureString
+                    .substring(millisInFutureString.length()-7, millisInFutureString.length()-6));
+        else if(millisInFutureString.length()==8)
+            hours = Integer.parseInt(millisInFutureString
+                    .substring(millisInFutureString.length()-8, millisInFutureString.length()-6));
+        Log.i("INTS", "h "+hours+" m "+minutes+" s "+seconds);
+        mMillisInFuture += hours * 60;
+        mMillisInFuture += minutes * 60;
+        mMillisInFuture += seconds;
+        mMillisInFuture = mMillisInFuture * INTERVAL;
     }
 }
